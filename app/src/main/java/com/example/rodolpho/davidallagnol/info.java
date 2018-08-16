730a88c576454a5a6c98f487577c6f09a4a48c4a
Hannilock <31757372+Hannilock@users.noreply.github.com>
2018-08-16 15:22:31 -0300

    kfdzvh

--- app/src/main/java/com/example/rodolpho/davidallagnol/info.java
+++ app/src/main/java/com/example/rodolpho/davidallagnol/info.java
@@ -1,7 +1,9 @@
 package com.example.rodolpho.davidallagnol;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class info extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class info extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
     }
+
+    public void backk(View v){
+        Intent i = new Intent(this, Menu.class);
+        startActivity(i);
+    }
+
 }
+
+
--- app/src/main/java/com/example/rodolpho/davidallagnol/points.java
+++ app/src/main/java/com/example/rodolpho/davidallagnol/points.java
@@ -1,8 +1,10 @@
 package com.example.rodolpho.davidallagnol;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class points extends AppCompatActivity {
     RecyclerView listt;
@@ -14,5 +16,8 @@ public class points extends AppCompatActivity {
         listt = (RecyclerView) findViewById(R.id.listt);
     }
 
-
+    public void backk(View v){
+        Intent i = new Intent(this, Menu.class);
+        startActivity(i);
+    }
 }
